4f9863d5521575df81c39e6a5db5bc8c507f62e4
Eric Clemmons <eric@smarterspam.com>
2013-01-29 23:35:08 -0600

    Register ng-submit=loadRepos and RepoService

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
-function DemoController($scope) {
-  $scope.username = 'ericclemmons';
-};
+angular
+
+  // Registed via `ng-app`
+  .module('app', [
+    'ngResource'
+  ])
+
+  // Create a `RepoService` for use within controllers`
+  .factory('RepoService', function($resource) {
+    return $resource('https://api.github.com/users/:username/repos', {}, {
+      query: {
+        method: 'JSONP',
+        params: { callback: 'JSON_CALLBACK' }
+      }
+    });
+  })
+
+  // Registered via `ng-controller`
+  .controller('DemoController', function($scope, RepoService) {
+    $scope.username = 'ericclemmons';
+
+    $scope.loadRepos = function() {
+      $scope.repos = RepoService.query({ username: $scope.username });
+    };
+  })
+;
+
